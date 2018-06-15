9fdfeb85a374cb7e55aa873fb8c40258a250f104
rupii <mudavathrupesh@gmail.com>
2018-06-15 23:37:38 +0530

    electtion contract

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -1,8 +1,22 @@
-pragma solidity ^0.4.17;    
+pragma solidity ^0.4.2;
 
 contract Election{
-    string public candidate;
+
+    struct Candidate{
+        uint id;
+        string name;
+        uint votes;
+    }
+
+    mapping (uint=>Candidate) public candidates;
+    uint public candidateCount;
+
     constructor()public{
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        candidates[++candidateCount] = Candidate(candidateCount, _name, 0);
     }
 }
